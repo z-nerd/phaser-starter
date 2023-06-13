80c243f5c1d3e350dcb1029c6898be05c46fc734
Zero <zero.js.dev@gmail.com>
2023-06-13 20:25:14 +0330

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+## phaser starter
+A simple starter for phaser games
+
+<!-- 
+### Setup
+```bash
+yarn create vite phaser-starter --template vanilla-ts
+```
+
+```bash
+yarn
+
+yarn add phaser
+``` -->
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { Game, Types } from 'phaser'
+import { HelloWorldScene } from "./scenes"
+
+
+export type IGameConfig = Types.Core.GameConfig & {
+
+}
+
+
+const config: IGameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: [HelloWorldScene]
+}
+
+
+const game = new Game(config)
